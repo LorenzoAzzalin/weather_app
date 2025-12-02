7760bd0d8a9cb19b8740d3fd63858cf23f6b9c83
Lorenzo Azzalin <83167654+LorenzoAzzalin@users.noreply.github.com>
2025-12-02 01:46:14 +0100

    Update funzioni_marittime.js

--- funzioni_marittime.js
+++ funzioni_marittime.js
@@ -55,26 +55,35 @@ document.addEventListener("DOMContentLoaded", function () {
                     .openOn(map);
                 return;
             }
-            //Esegue un controllo per vedere se si è in un browser o in una webview, e in caso invia i dati a java
-            inviaDatiAlJava(lat, lng, data);
+            //Conoscere corpo marino in questione -> chiamata API per ragioni di efficienza -> scelta per cui si è optato
+            let nome;
+            let nomeAcqua = null;
+            ottieniNomeOceano_GeoNames(lat, lng, 'l.azzalin')
+              .then(nome => {
+                nomeAcqua = nome;
+                //SISTEMARE SINTASSI var html
+                const c = data.current;
+                const nd = v => (v == null ? "dato sconosciuto" : v);
+                const html = `
+                    <b>Dati Meteo-Marini Attuali</b><br><br>
+                    <b>Geolocalizzazione</b> ${nomeAcqua}<br>
+                    <b>Altezza onda</b>: ${nd(c.wave_height)} m<br>
+                    <b>Direzione onda</b>: ${nd(c.wave_direction)}°<br><br>
+                    <b>Direzione delle onde generate dal vento</b>: ${nd(c.wind_wave_direction)}&deg;<br>
+                    <b>Periodo delle onde generate dal vento</b>: ${nd(c.wind_wave_period)} s<br><br>
+                    <b>Temperatura della superficie del mare</b>: ${nd(c.sea_surface_temperature)} &deg;C<br>
+                    <b>Altezza del livello del mare (MSL)</b>: ${nd(c.sea_level_height_msl)} m<br><br>
+                    <b>Direzione corrente oceanica</b>: ${nd(c.ocean_current_direction)}°<br>
+                    <b>Velocità corrente oceano</b>: ${nd(c.ocean_current_velocity)} m/s<br>
+                `;
+                L.popup()
+                    .setLatLng([lat, lng])
+                    .setContent(html)
+                    .openOn(map);
 
-            const c = data.current;
-            const nd = v => (v == null ? "dato sconosciuto" : v);
-            const html = `
-                <b>Dati Meteo-Marini Attuali</b><br><br>
-                <b>Altezza onda</b>: ${nd(c.wave_height)} m<br>
-                <b>Direzione onda</b>: ${nd(c.wave_direction)}°<br><br>
-                <b>Direzione delle onde generate dal vento</b>: ${nd(c.wind_wave_direction)}&deg;<br>
-                <b>Periodo delle onde generate dal vento</b>: ${nd(c.wind_wave_period)} s<br><br>
-                <b>Temperatura della superficie del mare</b>: ${nd(c.sea_surface_temperature)} &deg;C<br>
-                <b>Altezza del livello del mare (MSL)</b>: ${nd(c.sea_level_height_msl)} m<br><br>
-                <b>Direzione corrente oceanica</b>: ${nd(c.ocean_current_direction)}°<br>
-                <b>Velocità corrente oceano</b>: ${nd(c.ocean_current_velocity)} m/s<br>
-            `;
-            L.popup()
-                .setLatLng([lat, lng])
-                .setContent(html)
-                .openOn(map);
+                //Esegue un controllo per vedere se si è in un browser o in una webview, e in caso invia i dati a java
+                inviaDatiAlJava(lat, lng, data, nomeAcqua);
+              }).catch(errore => console.error(errore));
         });
     });
 
@@ -101,7 +110,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const resp = await fetch(url);
             if (!resp.ok) throw new Error(`Errore HTTP! status: ${resp.status}`);
             const data = await resp.json();
-            console.log("=== Dati meteo marino ===");
+            console.log("Dati meteo marino");
             console.log(data);
             return data;
         } catch (err) {
@@ -109,12 +118,29 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 });
+
 //Funzione per il passaggio dei miei alla webview java
-function inviaDatiAlJava(lat, lng, data) {
-    const meteoJson = JSON.stringify(data);
+function inviaDatiAlJava(lat, lng, data, localita) {
+    //append di localita a data per geolocalizzazione
+    const dataConLocalita = {
+        ...data,
+        localita: localita || null
+    };
+    const meteoJson = JSON.stringify(dataConLocalita);
+    //console.log("MeteoJSON: " +  meteoJson);
     if (window.AndroidBridge && typeof window.AndroidBridge.riceviDati === "function") {
         AndroidBridge.riceviDati(lat.toString(), lng.toString(), meteoJson);
     } else {
         console.log("AndroidBridge non disponibile, probabilmente sei in browser");
     }
+}
+
+//Funzione per conoscere corpo marino
+async function ottieniNomeOceano_GeoNames(latitudine, longitudine, nomeUtente) {
+  const url = `http://api.geonames.org/oceanJSON?lat=${encodeURIComponent(latitudine)}&lng=${encodeURIComponent(longitudine)}&username=${encodeURIComponent(nomeUtente)}`;
+  const risposta = await fetch(url);
+  if (!risposta.ok) throw new Error('Errore GeoNames: ' + risposta.status);
+  const datiJson = await risposta.json();
+  // datiJson.ocean.name contiene il nome, se presente
+  return datiJson.ocean ? datiJson.ocean.name : null;
 }
